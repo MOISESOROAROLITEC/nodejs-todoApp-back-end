7600be507b9eb215b3e8dafb9f67e2886d694a50
MOISESOROAROLITEC <moisesoro@arolitec.com>
2023-03-27 15:50:13 +0000

    Add a minddleware, user should use token to update his signin data

---
+++ minddlewares/user.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+
+const authantification = async (req, res, next) => {
+	try {
+		const authToken = req.header("Authorization").replace("Bearer ", "");
+		// console.log("token : ", authToken);
+		const decodeToken = jwt.verify(authToken, "theSecretKey");
+		const user = User.findOne({ _id: decodeToken._id, "tokens.token": authToken });
+		if (!user) throw new Error();
+		req.user = user;
+		next();
+	} catch (error) {
+		return res.status(401).json({ message: "signin before update your account" })
+	}
+}
+
+module.exports = authantification;
--- route/user.js
+++ route/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { signup, signin, update, get } = require("../controller/user");
+const authantification = require("../minddlewares/user");
 
 const userRoutes = express.Router();
 
 userRoutes.post("/user/signup", signup);
 userRoutes.post("/user/signin", signin);
 userRoutes.get("/user/get/:id", get);
-userRoutes.patch("/user/update/:id", update);
+userRoutes.patch("/user/update/:id", authantification, update);
 
 module.exports = userRoutes;
